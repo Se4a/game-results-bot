d125b1254b7555db9fc34ebf42edac7316af3b2d
Vladimir <87900298+Se4a@users.noreply.github.com>
2026-02-13 00:33:51 +0300

    Update __init__.py

--- bot/handlers/games/__init__.py
+++ bot/handlers/games/__init__.py
@@ -1,15 +1,15 @@
 from .csgo import register_csgo_handlers
 from .dota import register_dota_handlers
-from .valorant import register_valorant_handlers
-from .lol import register_lol_handlers
-from .wot import register_wot_handlers
-from .pubg import register_pubg_handlers
+# from .valorant import register_valorant_handlers
+# from .lol import register_lol_handlers
+# from .wot import register_wot_handlers
+# from .pubg import register_pubg_handlers
 
 __all__ = [
     'register_csgo_handlers',
     'register_dota_handlers',
-    'register_valorant_handlers',
-    'register_lol_handlers',
-    'register_wot_handlers',
-    'register_pubg_handlers'
+    # 'register_valorant_handlers',
+    # 'register_lol_handlers',
+    # 'register_wot_handlers',
+    # 'register_pubg_handlers'
 ]
